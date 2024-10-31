0367509e010844b001f5e36fd772de20025dd190
Frank <frankrcoleman@gmail.com>
2024-10-31 18:46:23 -0400

    unsplash api added

--- images_gallery/frontend/src/App.js
+++ images_gallery/frontend/src/App.js
@@ -3,13 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
   e.preventDefault();
-  console.log(word);
+    console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
 }
 
 
