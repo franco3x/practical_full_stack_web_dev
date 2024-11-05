4bbe855bc9eb4532d14dae8e196f6eefc8ef7fef
Frank <frankrcoleman@gmail.com>
2024-11-05 08:00:25 -0500

    first api call

--- images_gallery/frontend/src/App.js
+++ images_gallery/frontend/src/App.js
@@ -1,25 +1,30 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
-  e.preventDefault();
+    e.preventDefault();
     console.log(word);
     fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-}
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  };
 
-
-  
   return (
     <div className="App">
       <Header title="International Image Archive" />
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
